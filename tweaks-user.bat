f4b90e335d478c7dd9600950de5615e9a56851be
gitpel <22429622+gitpel@users.noreply.github.com>
2020-04-07 09:45:09 +0300

    f**king ms

--- tweaks-user.bat
+++ tweaks-user.bat
@@ -1,1 +1,1 @@
-@powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0Win10-Initial-Setup-Script\Win10.ps1" -include "%~dp0Win10-Initial-Setup-Script\Win10.psm1" -preset "%~dp0tweaks-user.bat"
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File "Win10-Initial-Setup-Script\Win10.ps1" -include "Win10-Initial-Setup-Script\Win10.psm1" -preset "tweaks-user.bat"
